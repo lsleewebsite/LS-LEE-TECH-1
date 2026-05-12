d95c33b67b2e51799c656451fcfb91d6b1215472
lsleewebsite <lsleewebsite@gmail.com>
2026-05-12 14:35:24 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/ls-lee-website/'  // Add this line - match your repo name
+  base: '/LS-LEE-TECH-WEBSITE/'  // Match your actual repo name!
 })
