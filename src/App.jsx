0dab2f8d17fcc94532b0fa256381287b6e409cf1
lsleewebsite <lsleewebsite@gmail.com>
2026-05-21 14:56:51 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react"
 import { Routes, Route, Navigate } from "react-router-dom"
 import Layout from "./Layout.jsx"
 import Home from "./pages/Home.jsx"
+import About from "./pages/About.jsx"
 import Industries from "./pages/Industries.jsx"
 import Services from "./pages/Services.jsx"
 import Projects from "./pages/Projects.jsx"
@@ -21,6 +22,7 @@ export default function App() {
     <Routes>
       <Route path="/" element={<Navigate to="/Home" replace />} />
       <Route path="/Home" element={<Wrap name="Home" Page={Home} />} />
+      <Route path="/About" element={<Wrap name="About" Page={About} />} />
       <Route path="/Industries" element={<Wrap name="Industries" Page={Industries} />} />
       <Route path="/Services" element={<Wrap name="Services" Page={Services} />} />
       <Route path="/Projects" element={<Wrap name="Projects" Page={Projects} />} />
