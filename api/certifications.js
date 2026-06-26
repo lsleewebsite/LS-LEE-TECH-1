190eabd28b256b187def8874f7edf7e1a2ea006d
lsleewebsite <lsleewebsite@gmail.com>
2026-06-26 13:51:14 +0800

    Update certifications.js

--- api/certifications.js
+++ api/certifications.js
@@ -46,6 +46,7 @@ export default async function handler(req, res) {
     if (!notionRes.ok) {
       return res.status(notionRes.status).json({
         error: 'Failed to fetch certifications from Notion',
+        details: data,
       })
     }
 
